ab399846ee9d20dd7b25e3eea60ff7934f48f88d
michedomingo <michedomingo@gmail.com>
2020-09-18 02:43:11 +0000

    Task2 solution

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API, returns a list containing titles of all hot articles
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], next_topic=''):
+    try:
+        posts = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'.
+                           format(subreddit, next_topic),
+                           allow_redirects=False,
+                           headers={'User-Agent': 'custom'})
+        if next_topic is None:
+            return hot_list
+        for topic in posts.json().get('data').get('children'):
+            hot_list += [topic.get('data').get('title')]
+        next_topic = posts.json().get('data').get('after')
+        recurse(subreddit, hot_list, next_topic)
+        return hot_list
+    except:
+        return None
